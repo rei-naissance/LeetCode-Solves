eb41ccccad32bb81476607547d1b54275a23df44
Philippe Andrei <110780576+rei-naissance@users.noreply.github.com>
2024-10-15 14:04:55 +0800

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 88 ms (60.82%), Space: 21.3 MB (45.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # get frequencies
         mp = Counter(nums)
-        keys = sorted(mp.items(), key = lambda x:x[1], reverse=True)
-        return [k for (k,v) in keys][:k]
+        # sort map according to the frequencies using lambda
+        mp = sorted(mp.items(), key = lambda x:x[1], reverse=True)
+        # return spliced list of keys
+        return [k for (k,v) in mp][:k]
 
-        # return [k for k,v in sorted(.items(), key = lambda x:x[1])]
+        # one liner
+        # return [k for k,v in sorted(Counter(nums).items(), key = lambda x:x[1], reverse=True)][:k]
