cc733d3506f3eac212a729c5aed8d7aee4ec0dbb
Philippe Andrei <110780576+rei-naissance@users.noreply.github.com>
2024-10-15 14:53:09 +0800

    0692-TOP-K-FREQUENT-WORDS Stats: Time: 60 ms (41.22%), Space: 16.8 MB (36.37%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,5 +1,11 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        
+        return [k for k,v in sorted(Counter(words).items(), key = lambda x:(-x[1], x))][:k]
+
+        # Record frequency of strings
         mp = Counter(words)
+        # Sorts the strings by their frequency in ascending order, then lexicographically
         mp = sorted(mp.items(), key=lambda x:(-x[1], x), reverse=False)
+        # Returns the spliced list
         return [k for k,v in mp][:k]
